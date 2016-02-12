60f86266c844c346a559e4e46e3de0c087b455cc
Tommi Jensen <tommi.jensen@gmail.com>
2016-02-12 14:44:58 +0100

    initial

---
+++ puppet/manifests/nodes.pp
@@ -0,0 +1,15 @@
+node "lampdemo" {
+  package{ 
+    ["php5-mysql", "php5-cli", "php5-gd", "php5-sqlite", "php5-common", "libapache2-mod-php5"]:
+    ensure => present,
+  }
+  file { "/var/www/index.html": 
+    ensure => absent,
+    require => Package["libapache2-mod-php5"]
+  }
+  file { "/var/www/index.php":
+    ensure => present,
+    content => "<?php phpinfo(); ?>",
+    require => Package["libapache2-mod-php5"]
+  }
+}
