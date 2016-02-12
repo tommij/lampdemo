9ba7d089b573e8e977032cf4453c384129e92081
Tommi Jensen <tommi.jensen@gmail.com>
2016-02-12 15:00:14 +0100

    placeholder for modules dir and mysql-server package added

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,6 +1,6 @@
 node "lampdemo" {
   package{ 
-    ["php5-mysql", "php5-cli", "php5-gd", "php5-sqlite", "php5-common", "libapache2-mod-php5"]:
+    ["php5-mysql", "php5-cli", "php5-gd", "php5-sqlite", "php5-common", "libapache2-mod-php5", "mysql-server"]:
     ensure => present,
   }
   file { "/var/www/index.html": 
